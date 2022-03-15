190347814695b57b328148b0e1bd844739e1e827
William Barkoff <william@barkoffusa.com>
2022-03-15 14:12:07 -0400

    add more functions

--- mapreduce.go
+++ mapreduce.go
@@ -1,6 +1,12 @@
 package gomapreduce
 
-func Equal[T any](a, b []T, cmp func(a, b T) bool) bool {
+// Equal takes two sices comparable with `==`, and returns true if they are equal when compared with ==.
+func Equal[T comparable](a, b []T) bool {
+	return EqualCmp(a, b, func(a, b T) bool { return a == b })
+}
+
+// EqualCmp takes two sices and a comparison function, and returns true if they are equal when each element is compared with the comparison function
+func EqualCmp[T any](a, b []T, cmp func(a, b T) bool) bool {
 	if len(a) != len(b) {
 		return false
 	}
@@ -12,7 +18,13 @@ func Equal[T any](a, b []T, cmp func(a, b T) bool) bool {
 	return true
 }
 
-func Map[A any, B any](f func(orig A, i int) B, arr []A) []B {
+// Map takes every value in a slice, applies `f` to it, and returns a new slice with the results.
+func Map[A any, B any](f func(e A) B, arr []A) []B {
+	return Mapi(func(e A, _ int) B { return f(e) }, arr)
+}
+
+// Map takes every value in a slice, applies `f` to it with the first argument as the value, and the second argument as the position in the slice, and returns a new slice with the results.
+func Mapi[A any, B any](f func(e A, i int) B, arr []A) []B {
 	result := []B{}
 	for i, val := range arr {
 		result = append(result, f(val, i))
--- mapreduce_test.go
+++ mapreduce_test.go
@@ -1,15 +1,28 @@
-package gomapreduce
+package gomapreduce_test
 
 import (
-	"reflect"
 	"strconv"
 	"testing"
+
+	"github.com/willbarkoff/gomapreduce"
 )
+
+func TestEqual(t *testing.T) {
+	ints_to_five := []int{1, 2, 3, 4, 5}
+	ints_to_four := []int{1, 2, 3, 4}
+	if !gomapreduce.Equal(ints_to_five, ints_to_five) {
+		t.Fail()
+	}
+	if gomapreduce.Equal(ints_to_four, ints_to_five) {
+		t.Fail()
+	}
+
+}
 
 func TestMap(t *testing.T) {
 	ints_to_five := []int{1, 2, 3, 4, 5}
-	added_strings := Map(func(orig int, i int) string { return strconv.Itoa(orig + i) }, ints_to_five)
-	if !reflect.DeepEqual(added_strings, []string{"1", "3", "5", "7", "9"}) {
+	added_strings := gomapreduce.Mapi(func(orig int, i int) string { return strconv.Itoa(orig + i) }, ints_to_five)
+	if !gomapreduce.Equal(added_strings, []string{"1", "3", "5", "7", "9"}) {
 		t.Fail()
 	}
 }
